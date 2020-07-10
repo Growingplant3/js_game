451b7dca84fcc9d168adc805ca5f43f2bb7a6f91
下田　佳紀 <y.s@experience.local>
2020-07-10 23:53:36 +0900

    制作中1

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("jsが読み込まれた。");
+// var img_area = document.getElementsByClassName("img_area")
+// var red_image = document.getElementsByClassName("red_image");
+// var vermilion_image = document.getElementsByClassName("vermilion_image");
+// var light_blue_image = document.getElementsByClassName("light_blue_image");
+// var yellow_image = document.getElementsByClassName("yellow_image");
+// var green_image = document.getElementsByClassName("green_image");
+
+// css
+// for ( var i = -1; i < red_image.length; i++) {
+//   red_image[i].style.border = "1px solid black";
+// }
+// デバック
+// console.log(img_area);
+// console.log(red_image);
+// console.log(vermilion_image);
+// console.log(light_blue_image);
+// console.log(yellow_image);
+// console.log(green_image);
