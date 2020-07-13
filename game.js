79a36677313469562548d820ea2a4488aba4fa4b
下田　佳紀 <y.s@platinum.local>
2020-07-13 17:36:32 +0900

    バグの原因究明中

--- game.js
+++ game.js
@@ -127,63 +127,63 @@ var opened_cards_list = [];
 // 揃った図柄のidを格納する配列 = もうめくれないカードのidリスト
 var matched_ids = [];
 
+// ターンエンドフラグ
+var turn_end = true;
+
 // ゲームスタート
-// var judge = true;
 turn += 1;
 
 // クリックしたら?をめくる処理
-console.log("カードをめくる関数を開始しました"); // デバッグ
-function flip_the_card(element) {
-  // 既に合致したカードはめくれない
-  var choiced_card = element.currentTarget;
-  for (var i=0; i<matched_ids.length; i++) {
-    if (choiced_card.id == matched_ids[i]) {
-      console.log("もうそこのマスはめくれません"); // デバッグ
-      return;
-    };
-  };
-  // 同一通し番号はめくれない = 同一通し番号でなければめくる = 同じ枠を二度クリックしても無効
-  switch (turn) {
-    case 3:
-      if (opened_cards_list[0] == choiced_card.id || opened_cards_list[1] == choiced_card.id) {
-        console.log("3回目のめくりで失敗"); // デバッグ
+while (turn_end) {
+  turn_end = false;
+  var flip_the_card = (function flip_the_card(element) {
+    // 既に合致したカードはめくれない
+    console.log(element.target);
+    var choiced_card = element.target.parentNode;
+    for (var i=0; i<matched_ids.length; i++) {
+      if (choiced_card.id == matched_ids[i]) {
+        console.log("もうそこのマスはめくれません"); // デバッグ
         return;
       };
-    case 2:
-      if (opened_cards_list[0] == choiced_card.id) {
-        console.log("2回目のめくりで失敗"); // デバッグ
-        return;
-      };
-  };
-  switch (turn) {
-    case 3:
-      if (opened_cards_list[0] != choiced_card.id && opened_cards_list[1] != choiced_card.id ) {
-        console.log("3回目のめくりが成功"); // デバッグ
-        placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
-        opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 3回目
-        turn += 1;
-        console.log(opened_cards_list); // デバッグ
-        console.log("カードをめくる関数が終わりました"); // デバッグ
-        image_match_check(turn,opened_cards_list) // 引数を持って次の関数に移動
-      };
-    case 2:
-    if (opened_cards_list.length == 1 && opened_cards_list[0] != opened_cards_list[1]) {
-      console.log("2回目のめくりが成功"); // デバッグ
-      placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
-      opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 2回目
-      turn +=1;
-      console.log(opened_cards_list); // デバッグ
     };
-    case 1:
-    if (opened_cards_list.length == 0) {
-      console.log("1回目のめくりが成功"); // デバッグ
-      placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
-      opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 1回目
-      turn += 1
-      console.log(opened_cards_list); // デバッグ
+    // 同一通し番号はめくれない = 同一通し番号でなければめくる = 同じ枠を二度クリックしても無効
+    switch (turn) {
+      case 3:
+        if (opened_cards_list[0] == choiced_card.id || opened_cards_list[1] == choiced_card.id) {
+          console.log("3回目のめくりで失敗"); // デバッグ
+          return;
+        } else {
+          console.log("3回目のめくりが成功"); // デバッグ
+          placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
+          opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 3回目
+          turn += 1;
+          console.log(opened_cards_list); // デバッグ
+          console.log("カードをめくる関数が終わりました"); // デバッグ
+          image_match_check(turn,opened_cards_list) // 引数を持って次の関数に移動
+        };
+      case 2:
+        if (opened_cards_list[0] == choiced_card.id) {
+          console.log("2回目のめくりで失敗"); // デバッグ
+          return;
+        } else {
+          console.log("2回目のめくりが成功"); // デバッグ
+          placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
+          opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 2回目
+          turn += 1;
+          console.log(opened_cards_list); // デバッグ
+        };
+      case 1:
+        if (opened_cards_list.length == 0) {
+          console.log("1回目のめくりが成功"); // デバッグ
+          placed_images[choiced_card.id].src = color_pallete[choiced_card.id];
+          opened_cards_list.push(choiced_card.id); // 通し番号を配列に格納 1回目
+          turn += 1
+          console.log(opened_cards_list); // デバッグ
+        };
     };
-  };
+  });
 };
+console.log("カードをめくる関数を開始できます"); // デバッグ
 for (var i=0; i < td_tags.length; i++) {
   td_tags[i].addEventListener('click', flip_the_card, false); // 全てのtdをクリックした時に発火する
 };
@@ -206,6 +206,7 @@ var stay_opened = (function stay_opened(turn,opened_cards_list) {
     matched_ids.push(opened_cards_list[i]);
   };
   console.log("カードはそのままにする関数を発火しました"); // デバッグ
+  number_clear(turn,opened_cards_list);
 });
 
 // めくったカードを戻す処理
@@ -217,19 +218,19 @@ var reverse_the_card = (function reverse_the_card(turn,opened_cards_list) {
     for ( var i=0; i<opened_cards_list.length; i++) {
       placed_images[opened_cards_list[i]].src = question;
     };
-    console.log("時が動き出す！");
+    console.log("時は動き出す！");
+    number_clear(turn,opened_cards_list);
   });
   console.log("カードを戻す関数発火しました"); // デバッグ
 });
 
-while (turn == 10) { // 発火しない処置
-  // 通し番号を格納する配列を作る初期化する処理
-  while (turn == 5) {
-    function number_clear() {
-      opened_cards_list = [];
-      console.log("（通し番号を格納する）配列を初期化する関数発火しました"); // デバッグ
-    };
-    turn = 1;
-    break;
-  };
-};
+// 通し番号を格納する配列を作る初期化する処理
+var number_clear = (function number_clear(turn,opened_cards_list) {
+  turn = 0;
+  console.log("ターン数を初期化しました");
+  opened_cards_list = [];
+  console.log("（通し番号を格納する）配列を初期化する関数発火しました"); // デバッグ
+  turn_end = true;
+  console.log("めくる関数に戻ります");
+  flip_the_card();
+});
